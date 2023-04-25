87dad1fda6900e20da2434a875e9a96137fa7d11
bgdnvk <bnovykov@gmail.com>
2023-04-25 19:33:58 +0200

    comments

--- src/lib/data/users.ts
+++ src/lib/data/users.ts
@@ -1,12 +1,15 @@
+//import the sdk
 import RandomUserSDK from "randomuser-sdk-ts/src/main";
 import { ApiUserResponse } from "randomuser-sdk-ts/src/types/ApiUserResponse";
 
 export default async function getUsers(n: number): Promise<ApiUserResponse> {
-
     try {
+        //initiatiate the class
         const sdk = new RandomUserSDK()
+        //get the data using the sdk
         const res = await sdk.getRandomUsers(n)
-        return res;
+        //return the data or do something with it (you get a JSON back)
+        return res
     } catch (error) {
         console.error('Error fetching users:', error);
         throw error;
